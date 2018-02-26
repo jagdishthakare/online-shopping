baf5cd981fcf54b58cd07202f07d6a87fbd1e23a
Jagdish Thakare <jagdishthakare7@gmail.com>
2018-02-26 15:23:40 +0530

    CRUD Operation JUnit Test Case. 03-08

--- shoppingbackend/src/test/java/net/kzn/shoppingbackend/test/CategoryTestCase.java
+++ shoppingbackend/src/test/java/net/kzn/shoppingbackend/test/CategoryTestCase.java
@@ -25,7 +25,7 @@ public class CategoryTestCase {
 		categoryDAO = (CategoryDAO)context.getBean("categoryDAO");
 	}
 	
-	@Test
+/*	@Test
 	public void testAddCategory() {
 		
 		category = new Category();
@@ -35,6 +35,95 @@ public class CategoryTestCase {
 		category.setImageURL("CAT_1001.png");
 		
 		assertEquals("Successfully added a category inside the table",true,categoryDAO.add(category));
+	
+	}*/
+	
+	
+/*	@Test
+	public void testGetCategory() {
+		
+		category = categoryDAO.get(1);
+
+		assertEquals("Successfully fetched a single category from the table!","Television",category.getName());
+
+		
 	}
+	*/
+	
+/*	@Test
+	public void testUpdateCategory() {
+		
+		category = categoryDAO.get(1);
+		
+		category.setName("TV");
+
+		assertEquals("Successfully updated a single category in the table!",true,categoryDAO.update(category));
+
+		
+	}*/
+	
+/*@Test
+	public void testDeleteCategory() {
+		
+		category = categoryDAO.get(1);
+		
+		assertEquals("Successfully deleted a single category in the table!",true,categoryDAO.delete(category));
+
+		
+	}*/
+
+	
+/*	@Test
+	public void testListCategory() {
+		
+		assertEquals("Successfully fetched the list of categories from the table!",1,categoryDAO.list().size());
+
+		
+	}*/
+	
+	
+	@Test
+	public void testCRUDCategory(){
+		
+		// Add operation
+        category = new Category();
+		
+		category.setName("Laptop");
+		category.setDescription("This is some description for Laptop !");
+		category.setImageURL("CAT_1.png");
+		
+		assertEquals("Successfully added a category inside the table",true,categoryDAO.add(category));
+	
+		
+        category = new Category();
+		
+		category.setName("Television");
+		category.setDescription("This is some description for Television !");
+		category.setImageURL("CAT_2.png");
+		
+		assertEquals("Successfully added a category inside the table",true,categoryDAO.add(category));
+	
+		
+		// fetching and updating the category
+		category = categoryDAO.get(2);
+		
+		category.setName("TV");
+
+		assertEquals("Successfully updated a single category in the table!",true,categoryDAO.update(category));
+
+		
+		// delete the category
+		
+		assertEquals("Successfully deleted a single category in the table!",true,categoryDAO.delete(category));
+
+		
+		// fetching the list
+		
+		assertEquals("Successfully fetched the list of categories from the table!",1,categoryDAO.list().size());
+
+		
+	}
+	
+	
 	
 }
